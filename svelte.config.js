eb0137c59cf2c3fdc8cb0fbed3c2e86075898ca5
hugotiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-10-22 08:56:49 -0300

    chore(static-generation): add conditions for GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	// The fallback: '404.html' and the path: { base: ... process.env.BASE_PATH } are needed to host
+	// the site on GitHub Pages. Change them if it is going to be hosted somewhere else.
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	},
 	extensions: ['.svelte', '.svx'],
 	vitePlugin: {
 		inspector: {
