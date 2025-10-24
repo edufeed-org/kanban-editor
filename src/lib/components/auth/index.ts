64f7dd8eb87bc2fd43fa87bd67f378f4cc444738
hugotiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-10-24 11:57:11 -0300

    fix(auth-module): remove import of non existent component

--- src/lib/components/auth/index.ts
+++ src/lib/components/auth/index.ts
@@ -1,9 +1,7 @@
 import LoginSheet from "./LoginSheet.svelte";
 import ProfileEditor from "./ProfileEditor.svelte";
-import UserHeader from "./UserHeader.svelte";
 
 export {
 	LoginSheet,
     ProfileEditor,
-    UserHeader
 };
