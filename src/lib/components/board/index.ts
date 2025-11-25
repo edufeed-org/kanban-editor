f8d165e3769a44be61b7d65da70d731d1d8070bf
hugotiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-11-25 15:00:10 -0300

    refactor(follow): remove follow feature since complexity grows up

--- src/lib/components/board/index.ts
+++ src/lib/components/board/index.ts
@@ -1,3 +1,2 @@
 export { default as ShareDialog } from './ShareDialog.svelte';
 export { default as ShareButton } from './ShareButton.svelte';
-export { default as FollowButton } from './FollowButton.svelte';
